83a4a5e06e83d8bde743417bbfa79ea930d519e0
Ali Raafat <aliraafat775@gmail.com>
2025-07-08 07:36:11 +0300

    updated

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import path from "path";
-dotenv.config({path: path.resolve("./src/config/.env.dev")});
+dotenv.config({ path: path.resolve("./src/config/.env.dev") });
 import bootstrap from "./src/app.controller.js";
-import {runIo} from "./src/Modules/Socket/socket.controller.js";
+import { runIo } from "./src/Modules/Socket/socket.controller.js";
 const app = express();
 const port = process.env.PORT || 5000;
 bootstrap(app, express);
 const httpServer = app.listen(port, () =>
-  console.log(`Example app listening on port ${port}!`)
+    console.log(`Example app listening on port ${port}!`)
 );
 runIo(httpServer);
